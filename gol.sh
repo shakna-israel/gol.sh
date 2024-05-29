4496d79562f65c30a87f48994642ad9a9ab9dc2b
James Milne <s4b3r6@gmail.com>
2024-05-29 21:11:31 +1000

    # Write the zip of the generated git repository:

--- gol.sh
+++ gol.sh
@@ -4,3 +4,4 @@
 # GOL                                                 #
 #######################################################
 # This project is an art project of a kind.           #
+# The idea is that the files it generates can be read #
